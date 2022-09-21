f7b53edbf6010cbb7d8f794f8c3580dc1d3494ef
Axel Goblet <axel.goblet@bigdatarepublic.nl>
2022-09-21 08:44:42 +0200

    simplify manage task

--- core/dbt/task/manage.py
+++ core/dbt/task/manage.py
@@ -5,7 +5,6 @@ from .runnable import ManifestTask
 from dbt.exceptions import (
     warn_or_error,
 )
-from dbt.parser.manifest import ManifestLoader
 from dbt.adapters.factory import get_adapter
 from dbt.contracts.graph.parsed import (
     ParsedModelNode,
@@ -16,19 +15,17 @@ from dbt.contracts.project import PruneModelsAction
 class ManageTask(CompileTask):
     def run(self):
         ManifestTask._runtime_initialize(self)
-        selected_uids = self.manifest.nodes.keys()
+        models_in_codebase = self.manifest.nodes.keys()
         adapter = get_adapter(self.config)
 
         with adapter.connection_named("master"):
-            required_schemas = self.get_model_schemas(adapter, selected_uids)
+            required_schemas = self.get_model_schemas(adapter, models_in_codebase)
             self.populate_adapter_cache(adapter, required_schemas)
 
             adapter.clear_transaction()
             self.manage_schema(adapter)
 
     def manage_schema(self, adapter):
-        manifest = ManifestLoader.get_full_manifest(self.config)
-
         # Read config
         managed_schemas_actions_config: Dict[Tuple[str, str], str] = {
             (ms.database or "", ms.schema or ""): ms.prune_models or PruneModelsAction.SKIP
@@ -41,21 +38,21 @@ class ManageTask(CompileTask):
             )
             return
 
-        models_in_results: Set[Tuple[str, str, str]] = set(
+        models_in_codebase: Set[Tuple[str, str, str]] = set(
             (n.database, n.schema, n.name)
-            for n in manifest.nodes.values()
+            for n in self.manifest.nodes.values()
             if isinstance(n, ParsedModelNode)
         )
 
         for database, schema in managed_schemas_actions_config.keys():
-            available_models: Dict[Tuple[str, str, str], str] = {
+            models_in_database: Dict[Tuple[str, str, str], str] = {
                 (database, schema, relation.identifier): relation
                 for relation in adapter.list_relations(database, schema)
             }
 
-            if len(available_models) == 0:
+            if len(models_in_database) == 0:
                 warn_or_error(f"No modules in managed schema '{schema}' for database '{database}'")
-            should_act_upon = available_models.keys() - models_in_results
+            should_act_upon = models_in_database.keys() - models_in_codebase
             for (target_database, target_schema, target_identifier) in should_act_upon:
                 target_action = managed_schemas_actions_config[(target_database, target_schema)]
                 if target_action == PruneModelsAction.WARN:
@@ -64,7 +61,7 @@ class ManageTask(CompileTask):
                     )
                 elif target_action == PruneModelsAction.DROP:
                     adapter.drop_relation(
-                        available_models[(target_database, target_schema, target_identifier)]
+                        models_in_database[(target_database, target_schema, target_identifier)]
                     )
 
     def interpret_results(self, results):

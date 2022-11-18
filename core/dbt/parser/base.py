ca89141a9ae0416a97f0c171335c29e61a6d554a
Sung Won Chung <sungwonchung3@gmail.com>
2022-11-18 13:28:42 -0800

    backwards compatible nodeconfig

--- core/dbt/parser/base.py
+++ core/dbt/parser/base.py
@@ -305,12 +305,10 @@ class ConfiguredParser(
             else:
                 parsed_node.docs = Docs(show=docs_show)
 
-        # TODO: Do I need this here similar to what we did for node_color above?
         # If we have constraints_enabled in the config, copy to node level, for backwards
         # compatibility with earlier node-only config.
-        # if "constraints_enabled" in config_dict and config_dict["constraints_enabled"]:
-        #     parsed_node.constraints_enabled = config_dict["constraints_enabled"]
-        #     print("constraints_enabled: {}".format(parsed_node.constraints_enabled))
+        if "constraints_enabled" in config_dict and config_dict["constraints_enabled"]:
+            parsed_node.constraints_enabled = config_dict["constraints_enabled"]
 
         # unrendered_config is used to compare the original database/schema/alias
         # values and to handle 'same_config' and 'same_contents' calls

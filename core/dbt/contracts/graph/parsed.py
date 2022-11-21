5d06524c6facc0def65e6ea0348f4e288b0e0d62
Sung Won Chung <sungwonchung3@gmail.com>
2022-11-21 11:48:46 -0800

    fix grammar

--- core/dbt/contracts/graph/parsed.py
+++ core/dbt/contracts/graph/parsed.py
@@ -245,7 +245,7 @@ class ParsedNodeDefaults(NodeInfoMixin, ParsedNodeMandatory):
 
         if materialization_error or language_error or data_type_errors:
             raise CompilationException(
-                f"Only the SQL table materialization is supported for constraints \ndata_type values must defined for all columns and NOT be null or blank.\n  Materialization Error: {materialization_error}\n  Language Error: {language_error}\n  Data Type Errors: {data_type_errors}"
+                f"Only the SQL table materialization is supported for constraints \ndata_type values must be defined for all columns and NOT be null or blank.\n  Materialization Error: {materialization_error}\n  Language Error: {language_error}\n  Data Type Errors: {data_type_errors}"
             )
 
     # TODO: this is where we see the columninfo object display the data_type and constraint values

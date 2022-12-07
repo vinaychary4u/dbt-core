ce85c96ede9c899af52d21e63be19d6da9c18f17
Sung Won Chung <sungwonchung3@gmail.com>
2022-12-06 23:11:18 -0800

    add more passing tests

--- tests/adapter/dbt/tests/adapter/constraints/test_constraints.py
+++ tests/adapter/dbt/tests/adapter/constraints/test_constraints.py
@@ -2,6 +2,7 @@ import pytest
 from dbt.tests.util import (
     run_dbt,
     get_manifest,
+    run_dbt_and_capture
 )
 
 # Verify compilation errors show expected dynamic messages
@@ -14,8 +15,6 @@ from dbt.tests.util import (
 
 # Verify manfiest is updated with the correct constraints, data_types, and checks
 
-
-# Verify DDL matches an expectation
 
 # create a sql model fixture
 my_model_sql = """
@@ -52,92 +51,36 @@ models:
         data_type: date
 """
 
-# expected DDL
-my_model_expected_ddl = """
-create  table "dbt"."test16703921190279384893_test_constraints"."my_model__dbt_tmp"
-
-
-
-  (
-
-
-
-
-
-
-
-
-
-      id integer  not null  primary key  check (id > 0) ,
-
-
-
-
-
-
-
-
-
-      color string   ,
-
-
-
-
-
-
-
-
-
-      date_day date
-
-
-
+my_model_error_sql = """
+{{
+  config(
+    materialized = "view"
   )
-
- ;
-
-
-    insert into "dbt"."test16703921190279384893_test_constraints"."my_model__dbt_tmp"
-
-  (
-
-
-
-
-
-      id ,
-
-
-
-
-
-      color ,
-
-
-
-
-
-      date_day
-
-
-
-  )
-
-
-     (
-
-
-
+}}
 
 select
-
   1 as id,
-
   'blue' as color,
-
   cast('2019-01-01' as date) as date_day
+"""
 
-    );
+model_schema_errors_yml = """
+version: 2
+models:
+  - name: my_model
+    config:
+      constraints_enabled: true
+    columns:
+      - name: id
+        data_type: integer
+        description: hello
+        constraints: ['not null','primary key']
+        check: (id > 0)
+        tests:
+          - unique
+      - name: color
+        data_type: text
+      - name: date_day
 """
 
 
@@ -174,6 +117,40 @@ class TestModelLevelConstraintsEnabledConfigs(BaseConstraintsEnabledModelvsProje
 
         assert constraints_enabled_actual_config is True
 
-# Verify the DDL matches the expectation with an assert statement
 
 # If there is no schema config, show a compilation error
+class TestSchemaConstraintsEnabledConfigs(BaseConstraintsEnabledModelvsProject):
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "my_model.sql": my_model_sql,
+        }
+
+    def test__schema_error(self, project):
+        schema_error_expected = "Schema Error: `yml` configuration does NOT exist"
+        results, log_output = run_dbt_and_capture(['run'], expect_pass=False)
+        assert schema_error_expected in log_output
+
+
+class TestModelLevelConstraintsErrorMessages(BaseConstraintsEnabledModelvsProject):
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "my_model.sql": my_model_error_sql,
+            "constraints_schema.yml": model_schema_errors_yml,
+        }
+
+    def test__config_errors(self, project):
+
+        results, log_output = run_dbt_and_capture(['run'], expect_pass=False)
+        manifest = get_manifest(project.project_root)
+        model_id = "model.test.my_model"
+        my_model_config = manifest.nodes[model_id].config
+        constraints_enabled_actual_config = my_model_config.constraints_enabled
+
+        assert constraints_enabled_actual_config is True
+
+        expected_materialization_error = "Materialization Error: {'materialization': 'view'}"
+        expected_empty_data_type_error = "Columns with `data_type` Blank/Null Errors: {'date_day'}"
+        assert expected_materialization_error in log_output
+        assert expected_empty_data_type_error in log_output

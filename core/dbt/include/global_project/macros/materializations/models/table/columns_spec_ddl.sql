e46066ba515478d880108f91350dc22ba4183664
Sung Won Chung <sungwonchung3@gmail.com>
2023-01-17 10:12:09 -0800

    Revert "Update core/dbt/include/global_project/macros/materializations/models/table/columns_spec_ddl.sql"
    This reverts commit 59b02980dc26c22c52f67d4a07b19ac6aeec7cdc.

--- core/dbt/include/global_project/macros/materializations/models/table/columns_spec_ddl.sql
+++ core/dbt/include/global_project/macros/materializations/models/table/columns_spec_ddl.sql
@@ -30,20 +30,6 @@
 {% macro assert_columns_equivalent(sql) %}
   {# loop through user_provided_columns to get column names #}
     {%- set user_provided_columns = model['columns'] -%}
-    {%- set column_names_config_only = [] -%}
-    {%- for i in user_provided_columns -%}
-      {%- set col = user_provided_columns[i] -%}
-      {%- set col_name = col['name'] -%}
-      {%- set column_names_config_only = column_names_config_only.append(col_name) -%}
-    {%- endfor -%}
-    {%- set sql_file_provided_columns = get_columns_in_query(sql) -%}
-
-    {%- if column_names_config_only != sql_file_provided_columns -%}
-      {%- do exceptions.raise_compiler_error('Please ensure the name and order of columns in your `yml` file match the columns in your SQL file.\nSchema File Columns: ' ~ column_names_config_only ~ '\nSQL File Columns: ' ~ sql_file_provided_columns ~ ' ' ) %}
-    {%- endif -%}
-{% endmacro %}
-  {# loop through user_provided_columns to get column names #}
-    {%- set user_provided_columns = model['columns'] -%}
     {% set column_names_config_only = [] %}
     {% for i in user_provided_columns %}
       {% set col = user_provided_columns[i] %}

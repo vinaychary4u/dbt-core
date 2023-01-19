f74bd5cc09ea9d4873f76ca9eab515a5dfb0a74a
Mila Page <versusfacit@users.noreply.github.com>
2023-01-18 20:00:10 -0800

    I didn't like how pytest was running the fixture so wrapped it into a closure.

--- tests/functional/column_comments/test_column_comments.py
+++ tests/functional/column_comments/test_column_comments.py
@@ -44,15 +44,18 @@ class TestColumnComment:
 
     @pytest.fixture(scope="class")
     def run_has_comments(self, project):
-        run_dbt()
-        run_dbt(["docs", "generate"])
-        with open("target/catalog.json") as fp:
-            catalog_data = json.load(fp)
-        assert "nodes" in catalog_data
-        assert len(catalog_data["nodes"]) == 1
-        column_node = catalog_data["nodes"]["model.test.quote_model"]
-        column_comment = column_node["columns"]["2id"]["comment"]
-        assert column_comment.startswith("XXX")
+        def fixt():
+            run_dbt()
+            run_dbt(["docs", "generate"])
+            with open("target/catalog.json") as fp:
+                catalog_data = json.load(fp)
+            assert "nodes" in catalog_data
+            assert len(catalog_data["nodes"]) == 1
+            column_node = catalog_data["nodes"]["model.test.quote_model"]
+            column_comment = column_node["columns"]["2id"]["comment"]
+            assert column_comment.startswith("XXX")
+
+        return fixt
 
     def test_postgres_comments(self, run_has_comments):
-        run_has_comments
+        run_has_comments()

7bc1a663730ee1f91646d0c54920ae273644c75d
Mila Page <versusfacit@users.noreply.github.com>
2023-01-18 19:51:26 -0800

    Run black on test conversion

--- tests/functional/column_comments/test_column_comments.py
+++ tests/functional/column_comments/test_column_comments.py
@@ -31,10 +31,10 @@ class TestColumnComment:
     @pytest.fixture(scope="class")
     def project_config_update(self):
         return {
-            'models': {
-                'test': {
-                    'materialized': 'table',
-                    '+persist_docs': {
+            "models": {
+                "test": {
+                    "materialized": "table",
+                    "+persist_docs": {
                         "relation": True,
                         "columns": True,
                     },
@@ -45,14 +45,14 @@ class TestColumnComment:
     @pytest.fixture(scope="class")
     def run_has_comments(self, project):
         run_dbt()
-        run_dbt(['docs', 'generate'])
-        with open('target/catalog.json') as fp:
+        run_dbt(["docs", "generate"])
+        with open("target/catalog.json") as fp:
             catalog_data = json.load(fp)
-        assert 'nodes' in catalog_data
-        assert len(catalog_data['nodes']) == 1
-        column_node = catalog_data['nodes']['model.test.quote_model']
-        column_comment = column_node['columns']['2id']['comment']
-        assert column_comment.startswith('XXX')
+        assert "nodes" in catalog_data
+        assert len(catalog_data["nodes"]) == 1
+        column_node = catalog_data["nodes"]["model.test.quote_model"]
+        column_comment = column_node["columns"]["2id"]["comment"]
+        assert column_comment.startswith("XXX")
 
     def test_postgres_comments(self, run_has_comments):
         run_has_comments

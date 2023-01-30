f8b16bc4489f115837a01356ab4e8eb855ea9ca9
Dave Connors <dave.connors@fishtownanalytics.com>
2023-01-30 12:12:59 -0600

    adjust whitespace

--- core/dbt/include/global_project/macros/materializations/models/table/columns_spec_ddl.sql
+++ core/dbt/include/global_project/macros/materializations/models/table/columns_spec_ddl.sql
@@ -28,24 +28,24 @@
 {%- endmacro %}
 
 {% macro assert_columns_equivalent(sql) %}
-  {# loop through user_provided_columns to get column names #}
+  {#- loop through user_provided_columns to get column names -#}
     {%- set user_provided_columns = model['columns'] -%}
-    {% set column_names_config_only = [] %}
-    {% for i in user_provided_columns %}
-      {% set col = user_provided_columns[i] %}
-      {% set col_name = col['name'] %}
-      {% set column_names_config_only = column_names_config_only.append(col_name) %}
-    {% endfor %}
+    {%- set column_names_config_only = [] -%}
+    {%- for i in user_provided_columns -%}
+      {%- set col = user_provided_columns[i] -%}
+      {%- set col_name = col['name'] -%}
+      {%- set column_names_config_only = column_names_config_only.append(col_name) -%}
+    {%- endfor -%}
     {%- set sql_file_provided_columns = get_columns_in_query(sql) -%}
 
-    {# uppercase both schema and sql file columns #}
-    {% set column_names_config_upper= column_names_config_only|map('upper')|join(',')  %}
-    {% set column_names_config_formatted = column_names_config_upper.split(',')  %}
-    {% set sql_file_provided_columns_upper = sql_file_provided_columns|map('upper')|join(',') %}
-    {% set sql_file_provided_columns_formatted = sql_file_provided_columns_upper.split(',') %}
+    {#- uppercase both schema and sql file columns -#}
+    {%- set column_names_config_upper= column_names_config_only|map('upper')|join(',')  -%}
+    {%- set column_names_config_formatted = column_names_config_upper.split(',')  -%}
+    {%- set sql_file_provided_columns_upper = sql_file_provided_columns|map('upper')|join(',') -%}
+    {%- set sql_file_provided_columns_formatted = sql_file_provided_columns_upper.split(',') -%}
 
-    {% if column_names_config_formatted != sql_file_provided_columns_formatted %}
-      {% do exceptions.raise_compiler_error('Please ensure the name, order, and number of columns in your `yml` file match the columns in your SQL file.\nSchema File Columns: ' ~ column_names_config_formatted ~ '\nSQL File Columns: ' ~ sql_file_provided_columns_formatted ~ ' ' ) %}
-    {% endif %}
+    {%- if column_names_config_formatted != sql_file_provided_columns_formatted -%}
+      {%- do exceptions.raise_compiler_error('Please ensure the name, order, and number of columns in your `yml` file match the columns in your SQL file.\nSchema File Columns: ' ~ column_names_config_formatted ~ '\nSQL File Columns: ' ~ sql_file_provided_columns_formatted ~ ' ' ) %}
+    {%- endif -%}
 
 {% endmacro %}

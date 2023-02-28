4342a2a159a22caa8689c2a15412d146c1b378b9
Sam Debruyn <sam@debruyn.dev>
2023-02-28 15:58:27 +0100

    fix: add pytz dependency (#7080)

--- core/setup.py
+++ core/setup.py
@@ -65,6 +65,7 @@ setup(
         "dbt-extractor~=0.4.1",
         "typing-extensions>=3.7.4",
         "werkzeug>=1,<3",
+        "pytz>=2015.7",
         # the following are all to match snowflake-connector-python
         "requests<3.0.0",
         "idna>=2.5,<4",

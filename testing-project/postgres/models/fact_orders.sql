caeecf4a678e05a1420c338ca416e890b8256129
Callum McCann <cmccann51@gmail.com>
2023-03-09 10:29:27 -0800

    switching to postgres

--- testing-project/postgres/models/fact_orders.sql
+++ testing-project/postgres/models/fact_orders.sql
@@ -1,5 +1,9 @@
 select
-    * exclude order_date
+    order_id
+    ,order_country
+    ,order_total
+    ,had_discount
+    ,customer_id
     ,to_date(order_date,'MM/DD/YYYY') as order_date
     ,round(order_total - (order_total/2)) as discount_total
 from {{ref('fact_orders_source')}}
--- testing-project/postgres/models/values_testing.sql
+++ testing-project/postgres/models/values_testing.sql
@@ -1,3 +1,4 @@
+select 1 as test
 {# {% set mf = modules.mf %} #}
 {# {% set explain_plan = mf.explain(metrics=["sales"]) %} #}
 {# {% set explain_plan = mf.explain(self,metrics=["sales"]) %} #}

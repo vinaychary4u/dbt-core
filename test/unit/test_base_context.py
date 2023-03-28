ab1d181881eb6465c2c41343582a579792f3f654
Quigley Malcolm <quigley.malcolm@dbtlabs.com>
2023-03-28 16:12:40 -0700

    Add tests for logging jinja2.Undefined objects
    [CT-2259](https://github.com/dbt-labs/dbt-core/issues/7108) identifies
    an issue wherein dbt-core 1.0-1.3 raise errors if a jinja2.Undefined
    object is attempted to be logged. This generally happened in the form
    of `{{ log(undefined_variable, info=True) }}`. This commit adding this
    test exists for two reasons
    1. Ensure we don't have a regression in this going forward
    2. Exist as a commit to be used for backport fixes for dbt-core 1.0-1.3

---
+++ test/unit/test_base_context.py
@@ -0,0 +1,12 @@
+from dbt.context.base import BaseContext
+from jinja2.runtime import Undefined
+
+
+class TestBaseContext:
+    def test_log_jinja_undefined(self):
+        # regression test for CT-2259
+        try:
+            os.environ["DBT_ENV_SECRET_LOG_TEST"] = "cats_are_cool"
+            BaseContext.log(msg=Undefined(), info=True)
+        except Exception as e:
+            assert False, f"Logging an jinja2.Undefined object raises an exception: {e}"

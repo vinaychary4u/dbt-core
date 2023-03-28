1edbf64647c986d52d4a8a540489654e2d0a7fe2
Quigley Malcolm <quigley.malcolm@dbtlabs.com>
2023-03-28 16:23:14 -0700

    Add tests for checking `DBT_ENV_SECRET_`s don't break logging
    [CT-1783](https://github.com/dbt-labs/dbt-core/issues/6568) describes
    a bug in dbt-core 1.0-1.3 wherein when a `DBT_ENV_SECRET_` all
    `{{ log("logging stuff", info=True) }}` invocations break. This commit
    adds a test for this for two reasons:
    1. Ensure we don't regress to this behavior going forward
    2. Act as a base commit for making the backport fixes to dbt-core 1.0-1.3

--- test/unit/test_base_context.py
+++ test/unit/test_base_context.py
@@ -1,3 +1,5 @@
+import os
+
 from dbt.context.base import BaseContext
 from jinja2.runtime import Undefined
 
@@ -10,3 +12,11 @@ class TestBaseContext:
             BaseContext.log(msg=Undefined(), info=True)
         except Exception as e:
             assert False, f"Logging an jinja2.Undefined object raises an exception: {e}"
+
+    def test_log_with_dbt_env_secret(self):
+        # regression test for CT-1783
+        try:
+            os.environ["DBT_ENV_SECRET_LOG_TEST"] = "cats_are_cool"
+            BaseContext.log({"fact1": "I like cats"}, info=True)
+        except Exception as e:
+            assert False, f"Logging while a `DBT_ENV_SECRET` was set raised an exception: {e}"

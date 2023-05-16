14e0152576cedb8dbf974dc925961b29447c585b
Ian Knox <81931810+iknox-fa@users.noreply.github.com>
2023-05-16 13:50:28 -0500

    Missed PR fedback (#7642)
    (cherry picked from commit df23f68dd4e26a5fa19e5cbed4b9f2cf33e62a7d)

--- tests/functional/dependencies/test_local_dependency.py
+++ tests/functional/dependencies/test_local_dependency.py
@@ -8,6 +8,7 @@ import yaml
 
 from pathlib import Path
 from unittest import mock
+from contextlib import contextmanager
 
 import dbt.semver
 import dbt.config
@@ -16,6 +17,7 @@ import dbt.exceptions
 from dbt.tests.util import (
     check_relations_equal,
     run_dbt,
+    run_dbt_and_capture,
 )
 
 models__dep_source = """
@@ -83,6 +85,16 @@ macros__macro_override_schema_sql = """
 
 {%- endmacro %}
 """
+
+
+@contextmanager
+def up_one():
+    current_path = Path.cwd()
+    os.chdir("../")
+    try:
+        yield
+    finally:
+        os.chdir(current_path)
 
 
 class BaseDependencyTest(object):
@@ -172,6 +184,16 @@ class TestSimpleDependency(BaseDependencyTest):
             ["run", "--models", f"+{local_path}"],
         )
         assert len(results) == 2
+
+
+class TestSimpleDependencyRelativePath(BaseDependencyTest):
+    def test_local_dependency_relative_path(self, project):
+        last_dir = Path(project.project_root).name
+        with up_one():
+            _, stdout = run_dbt_and_capture(["deps", "--project-dir", last_dir])
+            assert (
+                "Installed from <local @ local_dependency>" in stdout
+            ), "Test output didn't contain expected string"
 
 
 class TestMissingDependency(object):

11f5e2a1ea1c1afcf7674574fc6e64e29b5b2de8
Pat Kearns <pat.kearns@dbtlabs.com>
2023-06-14 14:28:02 +1000

    remove set

--- core/dbt/include/global_project/macros/materializations/snapshots/snapshot_merge.sql
+++ core/dbt/include/global_project/macros/materializations/snapshots/snapshot_merge.sql
@@ -15,8 +15,8 @@
      and DBT_INTERNAL_DEST.dbt_valid_to is null
      and DBT_INTERNAL_SOURCE.dbt_change_type in ('update', 'delete')
         then update
-        set dbt_valid_to = DBT_INTERNAL_SOURCE.dbt_valid_to
-        set dbt_updated_at = DBT_INTERNAL_SOURCE.dbt_valid_to
+        set dbt_valid_to = DBT_INTERNAL_SOURCE.dbt_valid_to, 
+        dbt_updated_at = DBT_INTERNAL_SOURCE.dbt_valid_to
 
     when not matched
      and DBT_INTERNAL_SOURCE.dbt_change_type = 'insert'

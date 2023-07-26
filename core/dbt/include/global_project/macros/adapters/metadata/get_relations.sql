1686186dca4bbff19ab9dff60dbc059812831975
Mike Alfare <mike.alfare@dbtlabs.com>
2023-07-25 20:22:20 -0400

    merging main into feature

--- core/dbt/include/global_project/macros/adapters/metadata/get_relations.sql
+++ core/dbt/include/global_project/macros/adapters/metadata/get_relations.sql
@@ -1,7 +1,7 @@
-
 {% macro get_relations() %}
   {{ return(adapter.dispatch('get_relations', 'dbt')()) }}
 {% endmacro %}
+
 
 {% macro default__get_relations() %}
   {{ exceptions.raise_not_implemented(

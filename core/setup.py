8394bbd2789a8c892525d0d690ac40f3454e348a
Emily Rockman <emily.rockman@dbtlabs.com>
2023-08-15 19:56:46 -0500

    pin upper bound for sqlparse (#8236) (#8239)
    * pin upper bound for sqlparse
    
    * changelog
    # Conflicts:
    #	core/setup.py
    
    Co-authored-by: Grace Goheen <53586774+graciegoheen@users.noreply.github.com>

--- core/setup.py
+++ core/setup.py
@@ -59,7 +59,7 @@ setup(
         "networkx>=2.3,<2.8.1;python_version<'3.8'",
         "networkx>=2.3,<3;python_version>='3.8'",
         "packaging>20.9",
-        "sqlparse>=0.2.3",
+        "sqlparse>=0.2.3,<0.5",
         "dbt-extractor~=0.4.1",
         "typing-extensions>=3.7.4",
         "werkzeug>=1,<3",

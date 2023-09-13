c48e34c47a1eddf8d298bfae3b48fecc0b84ebbb
Gerda Shank <gerda@dbtlabs.com>
2023-09-13 10:46:00 -0400

    Add additional functional test for unit testing selection, artifacts, etc (#8639)

--- tests/functional/unit_testing/test_unit_testing.py
+++ tests/functional/unit_testing/test_unit_testing.py
@@ -1,5 +1,6 @@
 import pytest
-from dbt.tests.util import run_dbt
+from dbt.tests.util import run_dbt, write_file, get_manifest, get_artifact
+from dbt.exceptions import DuplicateResourceNameError
 
 my_model_sql = """
 SELECT
@@ -86,6 +87,11 @@ unit:
               - {id: 1, string_b: b}
         expect:
           - {string_c: ab}
+        config:
+           tags: test_this
+"""
+
+datetime_test = """
       - name: test_my_model_datetime
         given:
           - input: ref('my_model_a')
@@ -106,7 +112,7 @@ class TestUnitTests:
             "my_model.sql": my_model_sql,
             "my_model_a.sql": my_model_a_sql,
             "my_model_b.sql": my_model_b_sql,
-            "test_my_model.yml": test_my_model_yml,
+            "test_my_model.yml": test_my_model_yml + datetime_test,
         }
 
     @pytest.fixture(scope="class")
@@ -114,9 +120,55 @@ class TestUnitTests:
         return {"vars": {"my_test": "my_test_var"}}
 
     def test_basic(self, project):
-        run_dbt(["deps"])
         results = run_dbt(["run"])
         assert len(results) == 3
 
+        # Select by model name
         results = run_dbt(["unit-test", "--select", "my_model"], expect_pass=False)
         assert len(results) == 5
+
+        # Test select by test name
+        results = run_dbt(["unit-test", "--select", "test_name:test_my_model_string_concat"])
+        assert len(results) == 1
+
+        # Select, method not specified
+        results = run_dbt(["unit-test", "--select", "test_my_model_overrides"])
+        assert len(results) == 1
+
+        # Select using tag
+        results = run_dbt(["unit-test", "--select", "tag:test_this"])
+        assert len(results) == 1
+
+        # Partial parsing... remove test
+        write_file(test_my_model_yml, project.project_root, "models", "test_my_model.yml")
+        results = run_dbt(["unit-test", "--select", "my_model"], expect_pass=False)
+        assert len(results) == 4
+
+        # Partial parsing... put back removed test
+        write_file(
+            test_my_model_yml + datetime_test, project.project_root, "models", "test_my_model.yml"
+        )
+        results = run_dbt(["unit-test", "--select", "my_model"], expect_pass=False)
+        assert len(results) == 5
+
+        manifest = get_manifest(project.project_root)
+        assert len(manifest.unit_tests) == 5
+        # Every unit test has a depends_on to the model it tests
+        for unit_test_definition in manifest.unit_tests.values():
+            assert unit_test_definition.depends_on.nodes[0] == "model.test.my_model"
+
+        # We should have a UnitTestNode for every test, plus two input models for each test
+        unit_test_manifest = get_artifact(
+            project.project_root, "target", "unit_test_manifest.json"
+        )
+        assert len(unit_test_manifest["nodes"]) == 15
+
+        # Check for duplicate unit test name
+        write_file(
+            test_my_model_yml + datetime_test + datetime_test,
+            project.project_root,
+            "models",
+            "test_my_model.yml",
+        )
+        with pytest.raises(DuplicateResourceNameError):
+            run_dbt(["unit-test", "--select", "my_model"])

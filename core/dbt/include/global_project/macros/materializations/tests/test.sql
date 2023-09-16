7b4be33a1b40fb913bc73df9f0dcc52d4cfa8d93
Mike Alfare <mike.alfare@dbtlabs.com>
2023-09-15 23:15:59 -0400

    removed unnecessary formatting changes

--- core/dbt/include/global_project/macros/materializations/tests/test.sql
+++ core/dbt/include/global_project/macros/materializations/tests/test.sql
@@ -29,27 +29,27 @@
 
   {% elif should_store_failures() %}
 
-      {% set identifier = model['alias'] %}
-      {% set old_relation = adapter.get_relation(database=database, schema=schema, identifier=identifier) %}
-      {% set target_relation = api.Relation.create(
-          identifier=identifier, schema=schema, database=database, type='table') %}
+    {% set identifier = model['alias'] %}
+    {% set old_relation = adapter.get_relation(database=database, schema=schema, identifier=identifier) %}
+    {% set target_relation = api.Relation.create(
+        identifier=identifier, schema=schema, database=database, type='table') -%} %}
 
-      {% if old_relation %}
-          {% do adapter.drop_relation(old_relation) %}
-      {% endif %}
+    {% if old_relation %}
+        {% do adapter.drop_relation(old_relation) %}
+    {% endif %}
 
-      {% call statement(auto_begin=True) %}
-          {{ create_table_as(False, target_relation, sql) }}
-      {% endcall %}
+    {% call statement(auto_begin=True) %}
+        {{ create_table_as(False, target_relation, sql) }}
+    {% endcall %}
 
-      {% do relations.append(target_relation) %}
+    {% do relations.append(target_relation) %}
 
-      {% set main_sql %}
-          select *
-          from {{ target_relation }}
-      {% endset %}
+    {% set main_sql %}
+        select *
+        from {{ target_relation }}
+    {% endset %}
 
-      {{ adapter.commit() }}
+    {{ adapter.commit() }}
 
   {% else %}
 

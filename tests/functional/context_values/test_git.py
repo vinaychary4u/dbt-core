dae675913379ca57085f616acfbc67616c29fd9f
Benoit Perigaud <8754100+b-per@users.noreply.github.com>
2023-09-25 18:21:05 +0200

    Add functional test

---
+++ tests/functional/context_values/test_git.py
@@ -0,0 +1,37 @@
+import os
+import subprocess
+import pytest
+
+from dbt.tests.util import run_dbt_and_capture
+
+# we use a macro to print the value and check the logs when testing
+on_run_start_macro_assert_git_branch = """
+{% macro assert_git_branch_name() %}
+    {{ log("git branch name: " ~ git_branch, 1) }}
+{% endmacro %}
+"""
+
+
+class TestContextGitValues:
+    @pytest.fixture(scope="class")
+    def macros(self):
+        return {
+            "assert_git_branch_name.sql": on_run_start_macro_assert_git_branch,
+        }
+
+    @pytest.fixture(scope="class")
+    def project_config_update(self):
+        return {
+            "on-run-start": "{{ assert_git_branch_name() }}",
+        }
+
+    def test_git_values(self, project):
+        os.chdir(project.project_root)
+        # Initialize a new git repository
+        subprocess.run(["git", "init"], check=True)
+        subprocess.run(["git", "checkout", "-b" "new_branch_for_testing"], check=True)
+        subprocess.run(["git", "add", "*"], check=True)
+        subprocess.run(["git", "commit", "-m", "commit to git"], check=True)
+
+        _, run_logs = run_dbt_and_capture(["run"])
+        assert "git branch name: new_branch_for_testing" in run_logs

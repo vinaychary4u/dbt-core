728a3cb84de702a19f8615dde612e985a337289f
Quigley Malcolm <quigley.malcolm@dbtlabs.com>
2023-10-10 14:40:17 -0700

    Add partial parsing support to SavedQuery nodes

--- tests/functional/saved_queries/test_saved_query_parsing.py
+++ tests/functional/saved_queries/test_saved_query_parsing.py
@@ -49,3 +49,54 @@ class TestSavedQueryParsing:
         assert not result.success
         validation_errors = [e for e in events if e.info.name == "MainEncounteredError"]
         assert validation_errors
+
+
+class TestSavedQueryPartialParsing:
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "saved_queries.yml": saved_queries_yml,
+            "schema.yml": schema_yml,
+            "fct_revenue.sql": fct_revenue_sql,
+            "metricflow_time_spine.sql": metricflow_time_spine_sql,
+        }
+
+    def test_saved_query_metrics_changed(self, project):
+        # First, use the default saved_queries.yml to define our saved_queries, and
+        # run the dbt parse command
+        runner = dbtTestRunner()
+        result = runner.invoke(["parse"])
+        assert result.success
+
+        # Next, modify the default saved_queries.yml to change a detail of the saved
+        # query.
+        modified_saved_queries_yml = saved_queries_yml.replace("simple_metric", "txn_revenue")
+        write_file(modified_saved_queries_yml, project.project_root, "models", "saved_queries.yml")
+
+        # Now, run the dbt parse command again.
+        result = runner.invoke(["parse"])
+        assert result.success
+
+        # Finally, verify that the manifest reflects the partially parsed change
+        manifest = result.result
+        saved_query = manifest.saved_queries["saved_query.test.test_saved_query"]
+        assert len(saved_query.metrics) == 1
+        assert saved_query.metrics[0] == "txn_revenue"
+
+    def test_saved_query_deleted_partial_parsing(self, project):
+        # First, use the default saved_queries.yml to define our saved_query, and
+        # run the dbt parse command
+        runner = dbtTestRunner()
+        result = runner.invoke(["parse"])
+        assert result.success
+        assert "saved_query.test.test_saved_query" in result.result.saved_queries
+
+        # Next, modify the default saved_queries.yml to remove the saved query.
+        write_file("", project.project_root, "models", "saved_queries.yml")
+
+        # Now, run the dbt parse command again.
+        result = runner.invoke(["parse"])
+        assert result.success
+
+        # Finally, verify that the manifest reflects the deletion
+        assert "saved_query.test.test_saved_query" not in result.result.saved_queries
